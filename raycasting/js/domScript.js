fcc70fd3e48546e7f3ab9b436c1cae24eac00e2f
pravinpoudel <prvnpoudel4@gmail.com>
2020-08-27 20:46:29 +0545

    menubar added to the main layout

---
+++ raycasting/js/domScript.js
@@ -0,0 +1,22 @@
+const burgerSlide = () => {
+  const burgerElement = document.getElementsByClassName("burger")[0];
+  const menuList = document.getElementsByClassName("menu-list")[0];
+  const listItem = document.querySelectorAll(".menu-list li");
+
+  burgerElement.addEventListener("click", function (event) {
+    console.log("i am clicked");
+    menuList.classList.toggle("nav-active");
+    burgerElement.classList.toggle("toggle");
+    listItem.forEach((element, index) => {
+      if (element.style.animation) {
+        element.style.animation = "";
+      } else {
+        element.style.animation = `menulistfade 0.5s ease forwards ${
+          index / 7 + 0.1
+        }s`;
+      }
+    });
+  });
+};
+
+burgerSlide();
